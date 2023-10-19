9f647bb2b2f32ea94ef411c6371dbfea0880e96c
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-10-19 10:07:37 +0200

    Modification de la déclaration du port et ajout des dependences

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
 
 
@@ -16,7 +15,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
 
 
 module.exports = app;
