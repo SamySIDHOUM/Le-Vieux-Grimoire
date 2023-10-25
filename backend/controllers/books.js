b50c711591346c1e992c84bbc9236acc7bdd46ae
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-10-25 20:56:56 +0200

    Ajustement des fonctions de gestion des livres

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,11 +1,15 @@
 const Book = require('../models/Books');
 
 exports.getAllBooks = (req, res, next) => {
-  
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.getOneBook = (req, res, next) => {
-  
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
 };
 
 exports.createBook = (req, res, next) => {
