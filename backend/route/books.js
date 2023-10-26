84315f5515310a0d5117c3508be68d1cd36a93c0
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-10-26 13:18:31 +0200

    Configuration de Multer pour le traitement des images

--- backend/route/books.js
+++ backend/route/books.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middlewars/auth');
+const multer = require('../middlewars/multer-config');
+
 
 const bookCtrl = require('../controllers/books');
 
-router.get('/', auth, bookCtrl.getAllBooks);
-router.get('/:id', auth, bookCtrl.getOneBook);
-router.post('/', auth, bookCtrl.createBook);
+router.get('/', bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
+router.post('/',auth, multer, bookCtrl.createBook);
 
 module.exports = router;
  
